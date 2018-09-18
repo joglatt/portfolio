17d700c09e2ba96a6ea373dd7922b32d30c4dcfc
Josh Glatt <joglatt@gmail.com>
2018-09-18 15:36:17 -0400

    fix

--- src/components/frame.js
+++ src/components/frame.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import Project from "./project.js";
 import "../App.css";
-import project1 from "./images/project1.png";
-import project2 from "./images/project2.png";
-import project3 from "./images/project3.png";
+import project1 from "../images/project1.PNG";
+import project2 from "../images/project2.PNG";
+import project3 from "../images/project3.PNG";
 class Frame extends Component {
   render() {
     const { projects } = this.props;
